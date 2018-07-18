86861ed927c3c883552ee59a233200f25c530075
James McTernan <jamesmct@comcast.net>
2018-07-18 00:01:49 -0700

    first commit with actual files and stuff (Oh Lord I misspelled Assignment)

---
+++ Assingment1Car/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment1Car {
+    class Car {
+
+        private string make;
+        public string Make {
+            get { return make; }
+            private set { make = value; }
+        }
+
+        private string model;
+        public string Model {
+            get { return model; }
+            private set { model = value; }
+        }
+
+        private int year;
+
+        public int Year {
+            get { return year; }
+            private set { year = value; }
+        }
+
+        static private int numCarsMade;
+
+        static public int NumCarsMade {
+            get { return numCarsMade; }
+            private set { numCarsMade = value; }
+        }
+
+
+        public const int MaxLegalSpeed=60;
+        public const int MinLegalSpeed=0;
+
+        private int currentSpeed;
+
+        public int CurrentSpeed {
+            get { return currentSpeed; }
+            private set { currentSpeed = value; }
+        }
+
+
+
+
+
+    }
+}
---
+++ Assingment1Car/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment1Car {
+    class Program {
+        static void Main(string[] args) {
+
+
+
+
+
+
+//            The car driving game: car has a speed, console displayed stuff: (A)ccelerate, (B)rake, (F)loor it, (S)lam the brakes, (P)ark
+//park only when stopped, speed can't go negative
+
+//Car constructors should update the static value of the number of cars made(doesn't track when cars are destroyed)
+//also has MAX_SPEED and MIN_SPEED
+
+////instance ctor (ctor tab- tab)
+//        public Car(string make, string model, int year, decimal price) {
+//            //initialize all the stuff
+//            this.make = make; make these readonly because they are only created once at object construction
+//this.model = model;
+//            this.year = year;
+//            this.price = price;
+
+//            Car.numberOfCarsMade++;
+//        }
+
+//        Static constructor is called before any other objects are made
+//static Car() {
+//            numberOfCars = 0;
+//        }
+
+//        parameter ctor, empty default ctor, deep copy ctor, and the static ctor(that isn't called by any code)
+//DisplayInfo shows all the info of the car, as well as the global
+        
+
+//        Properties
+
+//        prop -tab-tab
+//          propfull -tab-tab
+
+//public string Model {
+//            get { return model; }
+//            private set { model = value; }
+//        }
+
+//        private int size;
+//        public int Size {
+//            get { return size; }
+//            set {
+//                if (value < 0) { Size = 0; } else { Size = value; }
+//            }
+//        }
+
+
+    }
+    }
+}
