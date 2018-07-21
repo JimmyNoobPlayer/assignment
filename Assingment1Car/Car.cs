eb5f677fcea65ede0ea172205eae4af38f7e45e3
James McTernan <jamesmct@comcast.net>
2018-07-21 01:24:26 -0700

    small number of changes made 2018-7-20

--- Assingment1Car/Car.cs
+++ Assingment1Car/Car.cs
@@ -20,14 +20,12 @@ namespace Assingment1Car {
         }
 
         private int year;
-
         public int Year {
             get { return year; }
             private set { year = value; }
         }
 
         static private int numCarsMade;
-
         static public int NumCarsMade {
             get { return numCarsMade; }
             private set { numCarsMade = value; }
@@ -36,14 +34,46 @@ namespace Assingment1Car {
 
         public const int MaxLegalSpeed=60;
         public const int MinLegalSpeed=0;
+        public const string DefaultMake = "Ford";
+        public const string DefaultModel = "Pinto";
+        public const int DefaultYear = 1990;
 
         private int currentSpeed;
-
         public int CurrentSpeed {
             get { return currentSpeed; }
             private set { currentSpeed = value; }
         }
+        private string getAFeelingSoComplicated;
+        public string Feeling {
+            get { return getAFeelingSoComplicated; }
+            private set { getAFeelingSoComplicated = value; }
+        }
 
+
+        static Car() {
+            NumCarsMade = 0;
+        }
+
+        public Car(int year, string make, string model) {
+            NumCarsMade++;
+        }
+
+
+
+        public void setSpeed(int newSpeed) {
+            //possible feelings caused by the speed
+            private const string stopped = "Car is parked.";
+        private const string slow = "That guy behind you is getting angry, better speed up.";
+        private const string moderate = "If you move your arms really fast, it feels like you're running at an incredible speed!";
+        private const string tooFast = "Slow down!";
+
+            currentSpeed = newSpeed;
+            switch(
+
+        }
+        public void changeSpeed() {
+
+        }
 
 
 
